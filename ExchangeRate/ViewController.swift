2fcba07d76f6521c66fa4c1a67d5ec585da19047
19494115 <vvbugreev@sberbank.ru>
2023-01-18 10:35:15 +0300

    Initial Commit

---
+++ ExchangeRate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExchangeRate
+//
+//  Created by Бугреев Виктор Викторович on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
